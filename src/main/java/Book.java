4c810660ec3f26c9217ff9cb4f7a17a122023b15
Eman <137722003+123Emankh@users.noreply.github.com>
2025-12-01 14:34:06 +0200

    Add files via upload

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,53 +1,27 @@
+
 import java.time.LocalDate;
 
 public class Book {
-
-	private String title;
+    private String title;
     private String author;
     private String isbn;
     private boolean borrowed = false;
     private LocalDate dueDate;
+    private String borrowedBy;
 
-    
     public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn; 
     }
 
-    public String getTitle() { 
-    	return title;
-    	}
-    public String getAuthor() { 
-    	return author;
-    	}
-    public String getIsbn() {  
-    	return isbn; 
-    	}
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public String getIsbn() { return isbn.toLowerCase(); }
+    public boolean isBorrowed() { return borrowed; }
+    public LocalDate getDueDate() { return dueDate; }
+    public String getBorrowedBy() { return borrowedBy; }
 
-    /*@Override
-    public String toString() {
-        return title + " by " + author + " (ISBN: " + isbn + ")";
-    }*/
-	
-    @Override
-    public String toString() {
-        return isbn + " - " + title + " by " + author +
-               (borrowed ? " (Borrowed by " + borrowedBy + ", due " + dueDate + ")" : " (Available)");
-    }
-    
-    public boolean isBorrowed() { 
-    	
-    	return borrowed;
-    }
-    
-    public LocalDate getDueDate() { 
-    	
-    	return dueDate;
-    }
-    
-    private String borrowedBy; // memberId of borrower
-    
     public void borrow(String memberId) {
         borrowed = true;
         borrowedBy = memberId;
@@ -59,18 +33,22 @@ public class Book {
         dueDate = null; 
         borrowedBy = null;
     }
-    
-    public String getBorrowedBy() { 
-    	return borrowedBy;
-    	}
-    
-    
- // For testing: allow manual due date adjustment
+
     public void setDueDate(LocalDate dueDate) {
         this.dueDate = dueDate;
     }
-    
-    
-    
-    
+
+    public int getFinePerDay() {
+        return 10;
+    }
+
+    public int getLoanPeriodDays() {
+        return 28;
+    }
+
+    @Override
+    public String toString() {
+        return isbn + " - " + title + " by " + author +
+               (borrowed ? " (Borrowed by " + borrowedBy + ", due " + dueDate + ")" : " (Available)");
+    }
 }
