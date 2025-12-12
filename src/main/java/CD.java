5dbc3a206bdff3da2a16af3c46a01bb3a7353379
hoor-y <s12219440@stu.najah.edu>
2025-12-12 16:37:48 +0200

    test33

--- src/main/java/CD.java
+++ src/main/java/CD.java
@@ -129,7 +129,7 @@ public class CD {
             cd.borrowedBy = parts[3].substring("borrowed:".length());
             cd.dueDate = LocalDate.now().plusDays(7); // approximate reload
         }
-        return cd;  //testttttttttt
+        return cd;  //testttttttttt th
     }
     /**
      * Sets a custom due date for the CD.
